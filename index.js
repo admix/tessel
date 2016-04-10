7754a28f14f1729f7f8a6af52b6fc1a6af709875
admix <admix.snurnikov@gmail.com>
2016-04-10 11:05:32 -0400

    changed license. adding get requests

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // Import the interface to Tessel hardware
 var tessel = require('tessel');
+var http = require('http');
 
-// Turn one of the LEDs on to start.
-tessel.led[2].on();
+var options = {
+  host: 'https://www.google.ca',
+  path: '/search?q=hello'
+};
 
-// Blink!
-setInterval(function () {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
-}, 100);
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
+
+//
+// // Turn one of the LEDs on to start.
+// tessel.led[2].on();
+//
+// // Blink!
+// setInterval(function () {
+//   tessel.led[1].toggle();
+//   tessel.led[2].toggle();
+//   tessel.led[3].toggle();
+//   // tessel.led[4].toggle();
+// }, 100);
 
 console.log("I'm blinking! (Press CTRL + C to stop)");
